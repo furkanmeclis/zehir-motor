adf2adf95fc223687d12fc8b56c0856ff801eb73
Furkan Meclis <furkanmeclis@icloud.com>
2024-12-22 23:30:57 +0300

    updated

---
+++ public/build/assets/ApplicationLogo-CIDbvE4w.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-D2s52Jrn.js";function s(r){return o.jsx(o.Fragment,{children:o.jsx("img",{src:"/storage/images/logo.png",alt:"",...r})})}export{s as A};
--- public/build/assets/overlayservice.esm-B_Xi3Q_E.js
+++
@@ -1,1 +0,0 @@
-import{E as r}from"./app-BFo92QeC.js";var a=r();export{a as O};
